cf5b4d1c707aeddbe99ace8a641e1fd74d469f1a
zahrafitriani09 <168148478+zahrafitriani09@users.noreply.github.com>
2024-06-05 23:06:07 +0700

    Update line_chart.js
    BugFixed
    -Misahin Line chart jadi ada 2

--- line_chart.js
+++ line_chart.js
@@ -6,15 +6,134 @@ async function fetchData() {
   return await response.json();
 }
 
-// Function untuk inisialisasi chart
-function initializeChart(labels, datasets) {
-  const ctx = document.getElementById("line-chart").getContext("2d");
+function initializeChart(labels, revenueData, transactionData) {
+  const revenueCtx = document.getElementById("revenue-chart").getContext("2d");
+  const transactionCtx = document.getElementById("transaction-chart").getContext("2d");
 
-  currentChart = new Chart(ctx, {
+  currentChart = new Chart(revenueCtx, {
     type: "line",
     data: {
       labels: labels,
-      datasets: datasets,
+      datasets: [{
+        label: "Revenue",
+        data: revenueData,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        pointBackgroundColor: "rgba(75, 192, 192, 1)",
+        pointRadius: 3,
+        pointHoverRadius: 10 ,
+        pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
+      }],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true,
+          position: "top",
+          labels: {
+            font: {
+              size: 9,
+              color: "#666",
+            },
+          },
+        },
+        tooltip: {
+          bodyFont: {
+            size: 12,
+          },
+          callbacks: {
+            label: function (context) {
+              let label = context.dataset.label || "";
+              if (label) {
+                label += ": ";
+              }
+              if (
+                context.dataset.label === "Revenue" &&
+                context.parsed.y !== null
+              ) {
+                label += `$${context.parsed.y.toLocaleString()}`;
+              } else if (context.parsed.y !== null) {
+                label += context.parsed.y.toLocaleString();
+              }
+              return label;
+            },
+          },
+        },
+        datalabels: {
+          align: "end",
+          anchor: "end",
+          font: {
+            size: 9,
+          },
+          formatter: (value, context) => {
+            if (context.dataset.label === "Revenue") {
+              return `$${value.toLocaleString()}`;
+            } else {
+              return value.toLocaleString();
+            }
+          },
+        },
+      },
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            font: {
+              size: 10,
+              color: "#999",
+            },
+          },
+        },
+        y: {
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+          ticks: {
+            font: {
+              size: 8,
+              color: "#999",
+            },
+            callback: function (value) {
+              return value.toLocaleString();
+            },
+          },
+        },
+      },
+      elements: {
+        line: {
+          tension: 0.5,
+        },
+        point: {
+          radius: 3,
+          pointStyle: "circle",
+          borderWidth:1,
+        },
+      },
+    },
+    plugins: [ChartDataLabels], //menampilkan data label
+  });
+
+
+  currentChart2 = new Chart(transactionCtx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Transaction",
+        data: transactionData,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+        pointBackgroundColor: "rgba(255, 99, 132, 1)",
+        pointRadius: 3,
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: "rgba(255, 99, 132, 1)",
+      }],
     },
     options: {
       responsive: true,
@@ -115,37 +234,17 @@ async function updateChart() {
   const data = await fetchData();
   const selectedData = data[dataFilter];
 
-  const datasets = [
-    {
-      label: "Revenue",
-      data: selectedData.Revenue,
-      backgroundColor: "rgba(75, 192, 192, 0.2)",
-      borderColor: "rgba(75, 192, 192, 1)",
-      borderWidth: 1,
-      pointBackgroundColor: "rgba(75, 192, 192, 1)",
-      pointRadius: 3,
-      pointHoverRadius: 10 ,
-      pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
-    },
-    {
-      label: "Transaction",
-      data: selectedData.Transaction,
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgba(255, 99, 132, 1)",
-      borderWidth: 1,
-      pointBackgroundColor: "rgba(255, 99, 132, 1)",
-      pointRadius: 3,
-      pointHoverRadius: 10,
-      pointHoverBackgroundColor: "rgba(255, 99, 132, 1)",
-    },
-  ];
+  const labels = data.labels;
+  const revenueData = selectedData.Revenue;
+  const transactionData = selectedData.Transaction;
 
   // untuk menghapus chart sebelumnya
-  if (currentChart) {
+  if (currentChart && currentChart2) {
     currentChart.destroy();
+    currentChart2.destroy();
   }
 
-  initializeChart(data.labels, datasets);
+  initializeChart(labels, revenueData, transactionData);
 
   // Update scorecards
   document.getElementById("transaction-total").innerText =
@@ -155,6 +254,7 @@ async function updateChart() {
   document.getElementById("customer-total").innerText =
     selectedData.Customer.reduce((a, b) => a + b, 0).toLocaleString();
 }
+
 
 // Initialize the chart with the default dataset (Cumulative)
 document.addEventListener("DOMContentLoaded", async () => {
